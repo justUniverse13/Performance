7405426f54225200bd703fd7f1bfefcd3793de68
Bohdan Tsiunyk <bohdan.tsiunyk@gmail.com>
2020-09-04 11:24:23 +0300

    uod

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 rm -rf /Performance
+wget https://bitbucket.org/pjtr/jmeter-websocket-samplers/downloads/JMeterWebSocketSamplers-1.2.8.jar
+mv JMeterWebSocketSamplers-1.2.8.jar /mnt/jmeter/apache-jmeter-5.3/lib
 git clone https://github.com/justUniverse13/Performance
 /mnt/jmeter/apache-jmeter-5.3/bin/jmeter.sh -n -t /Performance/scenario.jmx  -l /mnt/scenario.jtl
