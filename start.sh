ecfc1c05a1d2bac0bcd0d2973bc4e0c51e32dc19
Bohdan Tsiunyk <bohdan.tsiunyk@gmail.com>
2020-09-04 10:33:02 +0300

    some updates

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
+rm -rf /Performance
 git clone https://github.com/justUniverse13/Performance
-/mnt/jmeter/apache-jmeter-5.3/bin/jmeter.sh -n -t /mnt/Performance/scenario.jmx  -l /mnt/scenario.jtl
+/mnt/jmeter/apache-jmeter-5.3/bin/jmeter.sh -n -t /Performance/scenario.jmx  -l /mnt/scenario.jtl
