a4bdb311b16f710969811759cd396dab8b6b1487
Bohdan Tsiunyk <44728281+justUniverse13@users.noreply.github.com>
2020-09-10 17:39:27 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo rm  /home/start.sh
+sudo cp /Performance/start.sh /home/
+sudo chmod 744  /home/start.sh
+cd /
 #rm -rf /Performance
 wget https://bitbucket.org/pjtr/jmeter-websocket-samplers/downloads/JMeterWebSocketSamplers-1.2.8.jar
 mv JMeterWebSocketSamplers-1.2.8.jar /mnt/jmeter/apache-jmeter-5.3/lib
