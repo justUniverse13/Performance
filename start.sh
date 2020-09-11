cc77da214a10ab0895ec4e7c5360a634414f2ecf
Bohdan Tsiunyk <bohdan.tsiunyk@gmail.com>
2020-09-11 12:01:17 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,15 @@ cd /
 #rm -rf /Performance
 wget https://bitbucket.org/pjtr/jmeter-websocket-samplers/downloads/JMeterWebSocketSamplers-1.2.8.jar
 mv JMeterWebSocketSamplers-1.2.8.jar /mnt/jmeter/apache-jmeter-5.3/lib
-git clone -b websockets https://github.com/justUniverse13/Performance
+
+echo " input  git clone ? >>>   1=   git  https://github.com/justUniverse13/Performance    2=  git clone -b websockets https://github.com/justUniverse13/Performance"
+read input
+if  $input == 1 ; then
+        echo " 1 =  git  https://github.com/justUniverse13/Performance"
+       sudo git  https://github.com/justUniverse13/Performance
+else
+        echo " 2 =  git clone -b websockets https://github.com/justUniverse13/Performance".
+        sudo git clone -b websockets https://github.com/justUniverse13/Performance
+
+#git clone -b websockets https://github.com/justUniverse13/Performance
 /mnt/jmeter/apache-jmeter-5.3/bin/jmeter.sh -n -t /Performance/scenario.jmx  -l /mnt/scenario.jtl
