4bcc15a1fa5b0f958bb07580c8ee2c64fc993de0
Bohdan Tsiunyk <bohdan.tsiunyk@gmail.com>
2020-09-11 12:08:55 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,8 +13,9 @@ if  $input == 1 ; then
         echo " 1 =  git  https://github.com/justUniverse13/Performance"
        sudo git  https://github.com/justUniverse13/Performance
 else
-        echo " 2 =  git clone -b websockets https://github.com/justUniverse13/Performance".
+        echo " 2 =  git clone -b websockets https://github.com/justUniverse13/Performance"
         sudo git clone -b websockets https://github.com/justUniverse13/Performance
 
 #git clone -b websockets https://github.com/justUniverse13/Performance
+sleep 15 
 /mnt/jmeter/apache-jmeter-5.3/bin/jmeter.sh -n -t /Performance/scenario.jmx  -l /mnt/scenario.jtl
